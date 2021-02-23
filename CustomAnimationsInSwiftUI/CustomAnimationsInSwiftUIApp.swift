2cbb04bc435a37001e6f767d0256514e821627df
Anu Mittal <anu22mittal@gmail.com>
2021-02-23 22:28:41 +0800

    Initial Commit

---
+++ CustomAnimationsInSwiftUI/CustomAnimationsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAnimationsInSwiftUIApp.swift
+//  CustomAnimationsInSwiftUI
+//
+//  Created by Anu Mittal on 23/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomAnimationsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
